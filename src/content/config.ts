19d07c3981eeb6d4e99969edf026b0c38cb388ac
Sergey Sova <mail@sergeysova.com>
2023-01-30 20:29:51 +0400

    Add draft support

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,6 +3,7 @@ import { z, defineCollection } from "astro:content";
 export const collections = {
   issues: defineCollection({
     schema: z.object({
+      draft: z.boolean().optional(),
       number: z.number(),
       introduction: z.string(),
       date: z.date(),
